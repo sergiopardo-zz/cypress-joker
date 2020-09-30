39647870ab8f8756a1ded37c6922fc89999059cd
Felipe Alvarado <mcdfelipea@gmail.com>
2020-09-30 17:25:21 -0500

    start the project doing a POC to get familiar with cypress

---
+++ README.md
@@ -0,0 +1,20 @@
+# Automation with CYPRESS
+
+## Pre requisites
+
+1. Install NVM
+2. Install a LTS version of NODE
+3. Install Cypress ```npm install cypress```
+
+## Run tests headless
+
+1. ```cd``` on the project folder
+2. Run ```npx cypress run --spec "cypress/integration/examples/login_runa.spec.js" --record --key 98c3a670-0c9b-404e-aa4c-698438442aaf```
+
+## Run test with UI
+
+1. ```cd``` on the project folder
+2. Run ```./node_modules/.bin/cypress open```
+3. In the UI select the browser and then select the spec file.
+4. MAGIC!!
+
---
+++ cypress/integration/examples/login_runa.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('User should login in Runa', () => {
+        cy.get(':nth-child(2) > .field-body > .field > .control > [data-testid=wrapped-input]')
+        .type('qa+runahr-api-br-feature-pt-398-m@runahr.com')
+        cy.get(':nth-child(3) > .field-body > .field > .control > [data-testid=wrapped-input]')
+        .type('runahr')
+        cy.get('[data-testid=login-submit]').click()
+        cy.get('[data-testid=modal-close-btn]').click()
+        cy.get('[data-testid=header-title]').contains('¡Hola, Administrador!')
+        cy.screenshot('login-runa')
+    })
+
+    it('User should get employees by API', function () {
+        cy.request({
+            method: 'get',
+            url: 'https://runahr-api-br-feature-pt-398-m.herokuapp.com/employees',
+            headers: {
+                'accept': 'application/json',
+                'authorization': '5_LOoITtGujtbPdZbAXzxAx3jPFieFNfP4iZ4j8GEaGogP2doFgwWcjltf6SVvrW'
+            }``
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+        })
+    })
+})
